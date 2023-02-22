03aed597cca7d8712b8b44fb4792d63eafc8ae38
AmanVernekar <amansuraj.av@gmail.com>
2023-02-22 15:28:43 +0000

    attempt 1 at adding ramp climbing ability
    Added error detection and correction to climb the ramp (kinda)

--- line_detect/line_detect.ino
+++ line_detect/line_detect.ino
@@ -7,16 +7,18 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *left = AFMS.getMotor(1);
 Adafruit_DCMotor *right = AFMS.getMotor(2);
 
-int lineFollower1 = A0;
-int lineFollower2 = A1;
-int lineFollower3 = A2;
-int lineFollower4 = A3;
+int green = A0;
+int orange = A1;
+int blue = A2;
+int yellow = A3;
 
 int lines[4];
 
 int mid_val = 500;
 int low_speed = 50;
-int high_speed = 200;
+int high_speed = 255;
+
+bool prev_dir_is_right;
 
 int ledPin = 13;
 int sensorValue = 0;
@@ -28,16 +30,16 @@ void setup() {
   Serial.println("Hello world!");
 
   AFMS.begin();
-  left->setSpeed(high_speed);
-  right->setSpeed(high_speed);
+  //left->setSpeed(high_speed);
+  //right->setSpeed(high_speed);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  lines[0] = (analogRead(lineFollower1) > mid_val);
-  lines[1] = (analogRead(lineFollower2) > mid_val);
-  lines[2] = (analogRead(lineFollower3) > mid_val);
-  lines[3] = (analogRead(lineFollower4) > mid_val);
+  lines[0] = (analogRead(orange) > mid_val);
+  lines[1] = (analogRead(yellow) > mid_val);
+  lines[2] = (analogRead(green) > mid_val);
+  lines[3] = (analogRead(blue) > mid_val);
   
   Serial.print(lines[0]);
   Serial.print(lines[1]);
@@ -45,27 +47,45 @@ void loop() {
   Serial.print(lines[3]);
   Serial.println("");
 
-  if (1==1) { // centre is on the white line (0 means black)
-    
-    if (lines[0] == 0 && lines[3] == 0){
-      left->setSpeed(high_speed);
-      right->setSpeed(high_speed);
-      left->run(BACKWARD);
-      right->run(BACKWARD);
-    }
+  if (lines[1] == 1 && lines[2] == 1){ //middle two sensors are on white line
+    left->setSpeed(high_speed);
+    right->setSpeed(high_speed);
+    left->run(BACKWARD);
+    right->run(BACKWARD);
+  }
 
-    else if (lines[0] == 0 && lines[3] == 1){ //right is on white
-      left->setSpeed(high_speed);
-      right->setSpeed(low_speed);
-      left->run(BACKWARD);
-      right->run(BACKWARD);
-    }
+  else if ((lines[1] == 0 && lines[2] == 1) || (lines[0] == 1)){ //turn right if middle-left is on black or outer-left is on white
+    left->setSpeed(high_speed);
+    right->setSpeed(low_speed);
+    left->run(BACKWARD);
+    right->run(BACKWARD);
+    prev_dir_is_right = true; //set to 0 if it has already turned right
+  }
 
-    else if (lines[0] == 1 && lines[3] == 0){ //left is on white
+  else if ((lines[1] == 1 && lines[2] == 0) || (lines[3] == 1)){ //turn left if middle-right is on black or outer-right is on white
+    left->setSpeed(low_speed);
+    right->setSpeed(high_speed);
+    left->run(BACKWARD);
+    right->run(BACKWARD);
+    prev_dir_is_right = false; //set to 1 if it has already turned left
+  }
+
+  else {
+    if (prev_dir_is_right){
+      //messed up by turning too much to the right so turn left
       left->setSpeed(low_speed);
       right->setSpeed(high_speed);
       left->run(BACKWARD);
       right->run(BACKWARD);
+      prev_dir_is_right = false;
+    }
+    else {
+      //messed up by turning too much to the left so turn right
+      left->setSpeed(high_speed);
+      right->setSpeed(low_speed);
+      left->run(BACKWARD);
+      right->run(BACKWARD);
+      prev_dir_is_right = true;
     }
   }
   //delay(2000);
