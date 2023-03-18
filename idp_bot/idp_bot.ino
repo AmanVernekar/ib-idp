cbc725a10c620c9a957b93011c220cea41cb3d89
AmanVernekar <amansuraj.av@gmail.com>
2023-03-18 23:08:13 +0000

    added more functionality
    robot can now pick up and drop blocks multiple times (tested)

--- idp_bot/idp_bot.ino
+++ idp_bot/idp_bot.ino
@@ -1,6 +1,3 @@
-// comments on lines 91, 101, 242-244, 281 should tell you what to play around with to make it work. use trial and error to make sure it turns back onto the line after a junction
-
-
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
@@ -23,24 +20,17 @@ int red_led = 4;
 int light_sensor = A1;
 int PUSH_BUTTON_SWITCH = 3;
 
-int lines[4];
+int lines[4]; // array to store line sensor readings
 
-int low_speed = 50;
+int low_speed = 0;
 int high_speed = 255;
 
 int junction_count = 0;
-int last_junc_time = 0;
+float last_junc_time = 0;
 bool block_picked = false;
 bool block_is_blue = false;
-bool returning_home = false;
 bool button_pressed = false;
 bool run_once = false;
-
-bool inside_box_once;
-float last_inside_box_once_time = 0;
-bool inside_box;
-float last_inside_box_time = 0;
-int box_counts = 0;
 
 void read_line_sensors();
 void start(bool left_dir);
@@ -56,6 +46,7 @@ void turn_ninety_l(int t);
 void turn_ninety_r(int t);
 void uturn();
 void line_follow();
+void junction_handler();
 void junction(bool left_dir);
 void junction_l();
 void junction_r();
@@ -85,112 +76,49 @@ void setup() {
 
 
 void loop() {
-  Serial.println(digitalRead(3));
-  if (button_pressed) {
+  if (button_pressed) { // button_pressed is set to true by the push button interrupt. This line ensures any code in loop() runs only once the push button has been pressed.
     read_line_sensors();
     
-    if (!run_once) {
+    if (!run_once) { // run the start code only once
       start_l();
     }
-    // if (millis() > 4.5*60*1000) {
-    //   return_home();
-    // }
-
-    bool junction_detected = (lines[1] == 1 && lines[2] == 1 && lines[3] == 1);
-    if (junction_detected) {
-      if (junction_count == 0) {
-        junction_r();      
-      }
-      else if (junction_count == 2) {
-        junction_r();    
-        junction_count = -1;  
-      }
-      if (millis() - last_junc_time > 2000) { // this is to ensure the same junction isn't counted multiple times. change 2000 if 2s seems too little/too much
-        junction_count++;
-      }
-      last_junc_time = millis();
-    }
-    else {
-      line_follow();
-    }
-
-    inside_box_once = (lines[0] == 0 && lines[1] == 0 && lines[2] == 0 && lines[3] == 0);
-    if (inside_box_once) {
-      float t = millis() - last_inside_box_time;
-      float t_once = millis() - last_inside_box_once_time;
-      if (t_once > 5000) {
-        box_counts = 0;
-      }
-      else if (t_once > 10 && t_once < 100) {
-        box_counts++;
-        last_inside_box_once_time = millis();
-      }
-    }
-
-    if (box_counts == 9) {
-      inside_box = true;
-      last_inside_box_time = millis();  
-    }
     
-    if (inside_box) { // the 3000 is to stop it from dropping the block soon after grabbing it. 
-      if (junction_count == 1) {
-        stop(100);
-        uturn();
-        stop(100);
-        if (block_is_blue && block_picked) {
-          drop();
-          start_l();
-        }
-        else {
-          start_r();
-        }
-      }
+    line_follow();
+
+    if (block_picked) {
+      myservo.write(180); // maintain the servo position when a block is held (in case the servo has moved slightly)
     }
 
-    if (!block_picked) {
-      digitalWrite(green_led, LOW);
-      digitalWrite(red_led, LOW);
-    }
-    else {
-      myservo.write(180);
-    }
   }
 }
 
 
 
 void read_line_sensors(){
-  lines[0] = digitalRead(red);
-  lines[1] = digitalRead(blue);
-  lines[2] = digitalRead(yellow);
-  lines[3] = digitalRead(green);
-  
-  // Serial.print(lines[0]);
-  // Serial.print(lines[1]);
-  // Serial.print(lines[2]);
-  // Serial.print(lines[3]);
-  // Serial.println("");
+  lines[0] = digitalRead(red); // extreme left
+  lines[1] = digitalRead(blue); // centre-left
+  lines[2] = digitalRead(yellow); // centre-right
+  lines[3] = digitalRead(green); // extreme right
 }
 
 void start(bool left_dir) {
   Serial.println("start code initiated");
-  myservo.write(0);
-  stop(2000);
+  myservo.write(0); // open grabber
   digitalWrite(blinky_led, HIGH);
   straight(high_speed, 2000);
-  while (lines[0] == 0 || lines[3] == 0) {
+  while (lines[0] == 0 || lines[3] == 0) { // continue moving forward as long as neither the extreme left nor the extreme right sensor sees the white line
     straight();
     read_line_sensors();
   }
-  delay(500);
-  if (left_dir) {
+  delay(500); // move ahead a little even after finding the white line to ensure better alignment
+  if (left_dir) { // turn onto the white line
     turn_ninety_l(1500);
   }
   else {
     turn_ninety_r(1500);
   }
   Serial.println("start code completed");
-  run_once = true;
+  run_once = true; // this ensures the start code is run only once eventhough it is in loop()
 }
 
 void start_l() {
@@ -206,7 +134,6 @@ void stop(int t=0) {
   right->setSpeed(0);
   digitalWrite(blinky_led, LOW);
   delay(t);
-  digitalWrite(blinky_led, HIGH);
 }
 
 void straight(int speed=high_speed, int t=0) {
@@ -214,6 +141,7 @@ void straight(int speed=high_speed, int t=0) {
   right->setSpeed(speed);
   left->run(FORWARD);
   right->run(FORWARD);
+  digitalWrite(blinky_led, HIGH);
   delay(t);
 }
 
@@ -222,6 +150,7 @@ void reverse(int speed=high_speed, int t=0) {
   right->setSpeed(speed);
   left->run(BACKWARD);
   right->run(BACKWARD);
+  digitalWrite(blinky_led, HIGH);
   delay(t); 
 }
 
@@ -230,6 +159,7 @@ void turn_right(int high=high_speed, int low=low_speed) {
   right->setSpeed(low);
   left->run(FORWARD);
   right->run(FORWARD);
+  digitalWrite(blinky_led, HIGH);
 }
 
 void turn_left(int high=high_speed, int low=low_speed) {
@@ -237,6 +167,7 @@ void turn_left(int high=high_speed, int low=low_speed) {
   right->setSpeed(high);
   left->run(FORWARD);
   right->run(FORWARD);
+  digitalWrite(blinky_led, HIGH);
 }
 
 void turn_ninety(bool left_dir, int t) {
@@ -250,6 +181,7 @@ void turn_ninety(bool left_dir, int t) {
     left->run(FORWARD);
     right->run(BACKWARD);
   }
+  digitalWrite(blinky_led, HIGH);
   delay(t);
 }
 
@@ -267,28 +199,56 @@ void uturn() {
 
 void line_follow() {
   if (lines[1] == 1 && lines[2] == 1){ //middle two sensors are on white line
-    straight();
+    if (lines[3] == 1) { // robot moves in one direction only and reverses to avoid the ramp so we care only about junctions on the right
+      junction_handler();
+    }
+    else {
+      straight();
+    }
   }
-  else if (lines[1] == 0 && lines[2] == 1) { // || (lines[0] == 1)){ //turn right if middle-left is on black
+  else if (lines[1] == 0 && lines[2] == 1) { // turn right if middle-left is on black
     turn_right();
   }
-  else if (lines[1] == 1 && lines[2] == 0) { // || (lines[3] == 1)){ //turn left if middle-right is on black
+  else if (lines[1] == 1 && lines[2] == 0) { // turn left if middle-right is on black
     turn_left();
   }
 }
 
+void junction_handler() {
+  Serial.print("Junction count. ");
+  Serial.println(junction_count);
+
+  if (millis() - last_junc_time > 1000) {
+    junction_count = junction_count + 1;
+    last_junc_time = millis();
+    // all junctions have a uniqiue index. 1 is where the blocks are picked up from, 2 is the green box, 3 is the starting area and 4 is the red box.
+  }
+
+  bool green_box = (junction_count == 2 && block_is_blue && block_picked);
+  bool red_box = (junction_count == 4 && !block_is_blue && block_picked);
+  if (junction_count == 1 || green_box || red_box) {
+    junction_r();
+    Serial.print("Turned at junction ");
+    Serial.println(junction_count);            
+  }
+
+  if (green_box || red_box) {
+    junction_count = 0; // reset once the block has been dropped
+  }
+}
+
 void junction(bool left_dir) {
-  int turn = 1850;  // change this if you want to change how much it turns into/out of a junction
-  int dist = 2000; // change this if you want to change how much it moves into/out of a junction
-  straight(high_speed, 900); // use this to change how much it moves before turning into a junction
+  int turn = 1850;  // how much it turns into/out of a junction
+  int dist = 2000; // how much it moves into/out of a junction
+  straight(high_speed, 900); // move straight even after detecting a junction before turning into it
   
   if (left_dir) {
-    turn_ninety_r(turn);
+    turn_ninety_r(turn); // turn into the junction
   }
   else {
     turn_ninety_l(turn);
   }
-  reverse(high_speed, dist);
+  reverse(high_speed, dist); // reverse into the junction
   stop();
   
   if (block_picked) {
@@ -298,8 +258,8 @@ void junction(bool left_dir) {
     grab();
   }
   
-  straight(high_speed, dist);
-  if (left_dir) {
+  straight(high_speed, dist); // leave the junction
+  if (left_dir) { // turn out of the junction
     turn_ninety_r(turn);
   }
   else {
@@ -317,7 +277,7 @@ void junction_r() {
 
 void grab() {
   myservo.write(180);
-  delay(500); // increase the delay if it still seems to be detecting colour too early
+  delay(1000); // delay to ensure the servo has reached the new position before initiating colour detection
   colour_detect();
 }
 
@@ -325,6 +285,8 @@ void drop() {
   myservo.write(0);
   block_picked = false;
   block_is_blue = false;
+  digitalWrite(green_led, LOW);
+  digitalWrite(red_led, LOW);
 }
 
 void colour_detect() {
@@ -334,7 +296,7 @@ void colour_detect() {
     light_readings[i] = analogRead(light_sensor);
   }
 
-  int median_light = median(light_readings);
+  int median_light = median(light_readings); // take the median of 10 readings
 
   if (median_light > 530) { //blue
     Serial.println("Blue");
@@ -345,9 +307,11 @@ void colour_detect() {
   }
   else if (median_light < 500) { //air
     Serial.println("Air");
-    drop();
-    delay(100);
-    grab(); // try again
+    block_picked = true;
+    block_is_blue = false;
+    drop(); // colour detection hasn't worked so we drop the block and pick it up again (hopefully in a slightly different orientation so that colour detection works)
+    delay(1000);
+    grab();
   }
   else { //brown
     Serial.println("Brown");
